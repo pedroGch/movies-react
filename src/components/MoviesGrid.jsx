e87561f79e8e5f82bebed54c7ac303f0bd9c2d64
pedro.gonzalezchavez <pedro.gonzalez@ucalp.edu.ar>
2022-01-14 19:14:39 -0300

    intro a los hooks

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,7 +1,20 @@
-import movies from "./movies.json"
+//import movies from "./movies.json"
+import { useEffect, useState } from "react";
 import styles from "./MoviesGrid.module.css"
 import {MovieCard} from "./MovieCard"
+
 export function MoviesGrid(){
+  const [movies, setMovies] = useState([]) 
+  
+  useEffect(() => [
+    fetch("https://api.themoviedb.org/3/discover/movie", {
+      headers:{
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjEwODRjMjk0NjRlZTlhMTNhYmExZDc3ZGM3ZTFlNyIsInN1YiI6IjYxZTFiYTgzYmYwZjYzMDA5YTE3NzQ5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.35cYaVexS2aEg5_SwIu48gE-31jThjf3GAwCt9MU46Y",
+        "Content-type": "application/json;charset=utf-8"
+      },
+    }).then((result) => result.json())
+      .then((data) => {setMovies(data.results)})
+  ], []);
   return (
     <ul className={styles.MovieGrid}>
       {movies.map((movie) => (
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import movie from "./movie.json"
 import styles from "./MovieDetails.module.css"
 export function MovieDetails(){
+  const {movieId} = useParams()
+  useEffectfect(() => {
+    effect
+    
+  }, []);
+
   const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
   return (
   <div className={styles.detailsContainer}>
